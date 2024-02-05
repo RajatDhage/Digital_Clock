9f420c0b39887c88c6f723903804be9ac2fb40ef
Rajat Dhage <104453729+RajatDhage@users.noreply.github.com>
2024-02-05 23:28:14 +0530

    v1

---
+++ digital_clock.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Color variables
+Blue=$'\e[1;34m'
+Yellow=$'\e[1;33m'
+Violet=$'\e[1;35m'
+
+# Function to clear the screen
+clear_screen() {
+    printf "\033c"
+}
+
+# Function to display the digital clock
+display_digital_clock() {
+    clear_screen
+    echo -e "${Yellow}*****************************"
+    echo -e "*${Violet}        Digital Clock${Yellow}        *"
+    echo -e "*${Blue}        $(date +%T)${Yellow}             *"
+    echo -e "*****************************${Reset}"
+}
+
+# Main loop
+while true
+do
+    display_digital_clock
+    sleep 1
+done
